e46d7b71b350323522253a5197e2d9b5cd65aab1
Drew Waltman <edwaltman@gmail.com>
2015-09-23 23:42:07 -0700

    Initial model, CLI interface fleshed out.  Version 0.1.

---
+++ pynewhall/model.py
@@ -0,0 +1,54 @@
+import json
+
+class Dataset:
+
+    def get_properties():
+        return self.properties
+
+    def get(key):
+        return self.ds_dict[key]
+
+    def set(key, value):
+        if key in self.properties:
+            self.ds_dict[key] = value
+        else:
+            raise Exception("Argument key is not a valid dataset property.")
+
+    def to_dict():
+        return self.ds_dict
+
+    def to_json():
+        # Return dataset as JSON string.
+        return json.dumps(self.ds_dict, sort_keys=True)
+
+    def __init__(self, input_dict, metadata_dict=None):
+        # Construct a dataset given input property dictionary.  Optionally specify
+        # a metadata dictionary (partial or complete) of properties.
+
+        self.properties = ["name", "country", "latitude", "longitude", "ns_hemisphere",
+                "ew_hemisphere", "elevation", "precipitation", "temperature", "start_year",
+                "end_year", "is_metric", "water_holding_capacity"]
+        self.extended_properties = ["station_name", "station_id", "station_elevation", 
+            "station_state_providence", "station_country", "mlra_name", "mlra_id", 
+            "contrib_first_name", "contrib_last_name", "contrib_title", "contrib_org",
+            "contrib_address", "contrib_city", "contrib_state_providence",
+            "contrib_postal", "contrib_country", "contrib_email", "contrib_phone",
+            "notes", "run_date", "model_version", "unit_system", "soil_air_offset",
+            "amplitude", "network"]
+        self.ds_dict = {}
+
+        try:
+            # Assemble local dictionary and raise exception on missing keys.
+            # These values must be present for model to function.
+            for key in self.properties:
+                self.ds_dict[key] = input_dict[key]
+            if metadata_dict:
+                # Non-critical metadata, treat the missing as empty strings.
+                for key in self.extended_properties:
+                    if key in metadata_dict:
+                        self.ds_dict[key] = metadata_dict[key]
+                    else:
+                        # Record an empty value for the key.
+                        self.ds_dict[key] = ""
+        except KeyError as ex:
+            raise Exception("Input is missing property: {}".format(ex))
---
+++ pynewhall/pynewhall.py
@@ -0,0 +1,52 @@
+import argparse, os
+
+from model import Dataset
+
+VERSION = 0.1
+
+def print_version():
+    print "PyNewhall {}".format(VERSION)
+
+def run_model(file_or_directory):
+    # Determine if input is single or multi run.
+    if os.path.isfile(file_or_directory):
+        # Single run mode, open file and run.
+        input_file = open(file_or_directory, "r")
+        # Parse into dataset.
+        dataset = Dataset({})
+        # Store report from model invocation on dataset.
+        # Check output selections.
+        # Write output file, close.
+        print "IMPLEMENT ME"
+        exit(-1)
+    elif os.path.isdir(file_or_directory):
+        print "IMPLEMENT ME"
+        exit(-1)
+    else:
+        # Not a file or directory, exit.
+        raise Exception("Not a file or directory: {}".format(file_or_directory))
+
+# Main interface.
+if __name__ == "__main__":
+    description = "PyNewhall {} - CLI Interface for the Newhall Simulation Model".format(
+        VERSION)
+
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("--version", help="show version and exit.", action="store_true")
+    parser.add_argument("--run", help="run model for argument file or directory (batch).")
+    args = parser.parse_args()
+
+    if args.version:
+        # Print version and exit succesfully.
+        print_version()
+        exit(0)
+
+    if args.run:
+        try:
+            # Attempt to run the model.
+            run_model(args.run)
+        except Exception as ex:
+            # Report exception message.
+            print ex
+            exit(1)
+        exit(0)
